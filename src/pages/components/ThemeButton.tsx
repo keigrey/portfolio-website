821d6da067eca3e65f2773f78d6f88dbd9c3e31c
keigrey <keixgrey@gmail.com>
2023-02-10 21:42:35 +0900

    feat: Change button title based on theme

--- src/pages/components/ThemeButton.tsx
+++ src/pages/components/ThemeButton.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { useTheme } from "next-themes";
 import { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import type { FC } from "react";
+
+// export async function getStaticProps({ locale }: { locale: string }) {
+//   return {
+//     props: {
+//       ...(await serverSideTranslations(locale, ["common"])),
+//     },
+//   };
+// }
 
 function ThemeButton({ mode }: { mode: string }) {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+
+  const { t } = useTranslation("common");
 
   useEffect(() => {
     setMounted(true);
@@ -16,8 +29,9 @@ function ThemeButton({ mode }: { mode: string }) {
 
   return (
     <>
+      <h1>{t("night")}</h1>
       <button onClick={() => setTheme(mode)}>
-        {mode === "dark" ? "NIGHT" : "DAY"}
+        {mode === "dark" ? t("night") : t("day")}
       </button>
     </>
   );
