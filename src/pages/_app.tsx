d404f733f1459dd87049aa75dfb30df927e2e8d7
keigrey <keixgrey@gmail.com>
2023-02-10 18:21:06 +0900

    feat: Wrap MyApp into appWithTranslation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import "@/styles/globals.css";
 import "../styles/theme.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { appWithTranslation } from "next-i18next";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
     </ThemeProvider>
   );
 }
+
+export default appWithTranslation(MyApp);
