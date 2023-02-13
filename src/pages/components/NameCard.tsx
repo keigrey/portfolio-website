5f7af16aac010fc31b5a465596f27d3a5e062351
keigrey <keixgrey@gmail.com>
2023-02-13 18:59:01 +0900

    feat: Add page links on NameCard

--- src/pages/components/NameCard.tsx
+++ src/pages/components/NameCard.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useTranslation } from "react-i18next";
 import LanguageSelector from "./LanguageSelector";
 import ThemeButton from "./ThemeButton";
@@ -6,18 +8,41 @@ import ThemeButton from "./ThemeButton";
 export default function NameCard() {
   const { t } = useTranslation("common");
 
+  const { asPath } = useRouter();
+  const dotSymbol = "○";
+
   return (
     <>
       <LanguageSelector />
       <h1>{t("name")}</h1>
       <h2>{t("role")}</h2>
       <ul>
-        <li>{t("home")}</li>
-        <li>{t("about")}</li>
-        <li>{t("resume")}</li>
-        <li>{t("projects")}</li>
-        <li>{t("hobbies")}</li>
-        <li>{t("info")}</li>
+        <li>
+          <Link href="/">{asPath === "/" ? dotSymbol : t("home")}</Link>
+        </li>
+        <li>
+          <Link href="/about">
+            {asPath === "/about" ? dotSymbol : t("about")}
+          </Link>
+        </li>
+        <li>
+          <Link href="/resume">
+            {asPath === "/resume" ? dotSymbol : t("resume")}
+          </Link>
+        </li>
+        <li>
+          <Link href="/projects">
+            {asPath === "/projects" ? dotSymbol : t("projects")}
+          </Link>
+        </li>
+        <li>
+          <Link href="/hobbies">
+            {asPath === "/hobbies" ? dotSymbol : t("hobbies")}
+          </Link>
+        </li>
+        <li>
+          <Link href="/info">{asPath === "/info" ? dotSymbol : t("info")}</Link>
+        </li>
       </ul>
       <ThemeButton mode="dark" />
       <ThemeButton mode="light" />
