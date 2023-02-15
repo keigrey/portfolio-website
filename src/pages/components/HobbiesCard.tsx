d1cd12f7f38c62b1ef1e8a2a69e64a0def2ee96b
keigrey <keixgrey@gmail.com>
2023-02-15 16:37:59 +0900

    feat: Create HobbiesCard template

---
+++ src/pages/components/HobbiesCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useTranslation } from "react-i18next";
+
+export default function HobbiesCard() {
+  const { t } = useTranslation("hobbies");
+
+  return (
+    <div className="hobbies-card">
+      <h1>{t("test")}</h1>
+    </div>
+  );
+}
