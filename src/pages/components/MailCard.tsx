a6622b0fcc335c3e8011f396f68e3d56d9caadb5
keigrey <keixgrey@gmail.com>
2023-02-15 16:38:10 +0900

    feat: Create MailCard template

---
+++ src/pages/components/MailCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useTranslation } from "react-i18next";
+
+export default function MailCard() {
+  const { t } = useTranslation("mail");
+
+  return (
+    <div className="mail-card">
+      <h1>{t("test")}</h1>
+    </div>
+  );
+}
