169237e407901ce11426b5f0b08bb34d566b8b85
keigrey <keixgrey@gmail.com>
2023-02-15 16:35:42 +0900

    feat: Create ResumeCard template

---
+++ src/pages/components/ProjectsCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useTranslation } from "react-i18next";
+
+export default function ProjectsCard() {
+  const { t } = useTranslation("projects");
+
+  return (
+    <div className="projects-card">
+      <h1>{t("test")}</h1>
+    </div>
+  );
+}
