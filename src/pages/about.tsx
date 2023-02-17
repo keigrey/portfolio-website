0f16e4bc5525ca770616ca9d266585db10779d8e
keigrey <keixgrey@gmail.com>
2023-02-17 18:08:00 +0900

    refactor: Change main tag to div for other page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -38,10 +38,10 @@ export default function About() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <div className="container">
         <NameCard />
         <AboutCard />
-      </main>
+      </div>
     </>
   );
 }
--- src/pages/hobbies.tsx
+++ src/pages/hobbies.tsx
@@ -38,10 +38,10 @@ export default function Hobbies() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <div className="container">
         <NameCard />
         <HobbiesCard />
-      </main>
+      </div>
     </>
   );
 }
