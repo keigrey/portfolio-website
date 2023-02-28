cadccd706349b02624da1bff133d9eec4f16622f
keigrey <keixgrey@gmail.com>
2023-02-28 17:03:44 +0900

    refactor: Remove vanta code from other components

--- src/pages/components/AboutCard.tsx
+++ src/pages/components/AboutCard.tsx
@@ -2,43 +2,12 @@ import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useTranslation } from "react-i18next";
-import { useState, useEffect, useRef } from "react";
-
-import FOG from "vanta/dist/vanta.fog.min";
-import * as THREE from "three";
 
 export default function AboutCard() {
   const { t } = useTranslation("about");
 
-  const [vantaEffect, setVantaEffect] = useState(null);
-  const vantaRef = useRef(null);
-
-  useEffect(() => {
-    if (!vantaEffect) {
-      setVantaEffect(
-        FOG({
-          el: vantaRef.current,
-          THREE,
-          mouseControls: true,
-          touchControls: true,
-          gyroControls: false,
-          minHeight: 200.0,
-          minWidth: 200.0,
-          highlightColor: 0xffffff,
-          midtoneColor: 0xaeaeae,
-          lowlightColor: 0x0,
-          baseColor: 0x0,
-          zoom: 0.6,
-        })
-      );
-    }
-    return () => {
-      if (vantaEffect) vantaEffect.destroy();
-    };
-  }, [vantaEffect]);
-
   return (
-    <div ref={vantaRef} className="about-card section-card">
+    <div className="about-card">
       <h1>About</h1>
       {/* <h1>{t("introduction")}</h1> */}
     </div>
