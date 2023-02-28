c26ed9dd1a72aed0b9db08cfeb1d1d14e20e1621
keigrey <keixgrey@gmail.com>
2023-02-28 17:03:00 +0900

    feat: Create separate SectionCard component

---
+++ src/pages/components/SectionCard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+
+import FOG from "vanta/dist/vanta.fog.min";
+import * as THREE from "three";
+
+export default function SectionCard(props) {
+  const [vantaEffect, setVantaEffect] = useState(null);
+  const vantaRef = useRef(null);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        FOG({
+          el: vantaRef.current,
+          THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          highlightColor: 0xffffff,
+          midtoneColor: 0xaeaeae,
+          lowlightColor: 0x0,
+          baseColor: 0x0,
+          zoom: 0.6,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
+  return (
+    <div ref={vantaRef} className="section-card">
+      {props.children}
+    </div>
+  );
+}
