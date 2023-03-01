ef6be9100bb8506cc03b4a08a5be7706b8e4931a
keigrey <keixgrey@gmail.com>
2023-03-01 15:27:36 +0900

    feat: Change vanta zoom value based on screen size

--- src/pages/components/SectionCard.tsx
+++ src/pages/components/SectionCard.tsx
@@ -5,11 +5,20 @@ import { useTheme } from "next-themes";
 import FOG from "vanta/dist/vanta.fog.min";
 import * as THREE from "three";
 
+interface WindowSizeInterface {
+  width: number | undefined;
+  height: number | undefined;
+}
+
 const getVantaEffectProperties = (
-  vantaReference,
-  currentTheme: string | undefined
+  vantaReference: any,
+  currentTheme: string | undefined,
+  windowSize: WindowSizeInterface
 ) => {
   const darkTheme = currentTheme === "dark";
+  const zoomValue =
+    // @ts-ignore
+    windowSize.height < 450 || windowSize.width < 769 ? 0.3 : 0.6;
 
   return {
     el: vantaReference.current,
@@ -20,34 +29,65 @@ const getVantaEffectProperties = (
     midtoneColor: darkTheme ? 0xaeaeae : 0xca2f1c,
     lowlightColor: darkTheme ? 0x0 : 0x6041ea,
     baseColor: darkTheme ? 0x0 : 0x82d18a,
-    zoom: 0.6,
+    zoom: zoomValue,
   };
 };
 
 export default function SectionCard(props) {
-  const [vantaEffect, setVantaEffect] = useState(null);
+  const [vantaEffect, setVantaEffect] = useState<any>(null);
   const vantaRef = useRef(null);
 
   const { theme, resolvedTheme } = useTheme();
 
-  useEffect(() => {
-    console.log("VAnta ran again");
-    console.log("THEME CHANGE", resolvedTheme);
+  const size = useWindowSize();
 
+  useEffect(() => {
     if (!vantaEffect) {
-      setVantaEffect(FOG(getVantaEffectProperties(vantaRef, resolvedTheme)));
+      setVantaEffect(
+        FOG(getVantaEffectProperties(vantaRef, resolvedTheme, size))
+      );
     } else {
-      setVantaEffect(FOG(getVantaEffectProperties(vantaRef, resolvedTheme)));
+      setVantaEffect(
+        FOG(getVantaEffectProperties(vantaRef, resolvedTheme, size))
+      );
     }
-  }, [theme]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [theme, size]);
 
   useEffect(() => {
     return () => vantaEffect?.destroy();
-  });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div ref={vantaRef} className="section-card">
       {props.children}
     </div>
   );
+}
+
+// Hook
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  const [windowSize, setWindowSize] = useState<WindowSizeInterface>({
+    width: undefined,
+    height: undefined,
+  });
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
 }
