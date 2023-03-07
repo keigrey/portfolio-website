fee7bba4875dc6e6d5ec86d6b6ccf4170735a184
keigrey <keixgrey@gmail.com>
2023-03-07 17:37:10 +0900

    feat: Display ProjectCardMini on /projects page

--- src/pages/components/ProjectsCard.tsx
+++ src/pages/components/ProjectsCard.tsx
@@ -2,13 +2,34 @@ import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useTranslation } from "react-i18next";
+import ProjectCardMini from "./ProjectCardMini";
 
 export default function ProjectsCard() {
   const { t } = useTranslation("projects");
 
+  const projectTitles = [
+    t("ondoTitle"),
+    t("talltowerTitle"),
+    t("appreciateTitle"),
+    t("taberuTitle"),
+  ];
+  const projectDescriptions = [
+    t("ondoDescription"),
+    t("talltowerDescription"),
+    t("appreciateDescription"),
+    t("taberuDescription"),
+  ];
+
   return (
     <div className="projects-card content-card">
-      <h1>{t("test")}</h1>
+      {projectTitles.map((title, index) => (
+        <ProjectCardMini
+          key={index}
+          number={index > 9 ? index + 1 : `0${index + 1}`}
+          title={title}
+          seeMore={t("seeMore")}
+        />
+      ))}
     </div>
   );
 }
