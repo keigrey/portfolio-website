5b22b67ca8b37efff5c5d041727fbcecceb677e8
keigrey <keixgrey@gmail.com>
2023-03-07 18:49:22 +0900

    feat: Open project description by clicking 'see more'

--- src/pages/components/ProjectCardMini.tsx
+++ src/pages/components/ProjectCardMini.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 
-export default function ProjectCardMini({ number, title, seeMore }) {
+export default function ProjectCardMini({
+  number,
+  title,
+  seeMore,
+  setProjectId,
+  projectId,
+}) {
   return (
-    <div className="project-card">
+    <div className="project-card-mini">
       <h2>{number}</h2>
       <div className="project-card-circle">
-        <p>{seeMore}</p>
+        <button onClick={() => setProjectId(projectId)}>{seeMore}</button>
+        {/* <p>{seeMore}</p> */}
       </div>
       <h1>{title}</h1>
     </div>
