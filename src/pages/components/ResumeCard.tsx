721878832d22817ac56494f3ecbb6c93beb950f9
keigrey <keixgrey@gmail.com>
2023-03-10 18:25:19 +0900

    feat: Add Satellite element to ResumeCard

--- src/pages/components/ResumeCard.tsx
+++ src/pages/components/ResumeCard.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useTranslation } from "react-i18next";
+import Satellite from "./Satellite";
 
 export default function ResumeCard() {
   const { t } = useTranslation("resume");
 
   return (
     <div className="resume-card content-card">
-      <h1>{t("test")}</h1>
+      {/* <h1>{t("test")}</h1> */}
+      <Satellite />
     </div>
   );
 }
