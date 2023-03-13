a7b44cec140c0102e3a7d8763f14431ee0ef0958
keigrey <keixgrey@gmail.com>
2023-03-13 17:04:11 +0900

    feat: Add svg arrow to ProjectCardMini

--- src/pages/components/ProjectCardMini.tsx
+++ src/pages/components/ProjectCardMini.tsx
@@ -9,12 +9,24 @@ export default function ProjectCardMini({
 }) {
   return (
     <div className="project-card-mini">
-      <h2>{number}</h2>
-      <div className="project-card-circle">
-        <button onClick={() => setProjectId(projectId)}>{seeMore}</button>
-        {/* <p>{seeMore}</p> */}
-      </div>
+      <h2>{number}.</h2>
       <h1>{title}</h1>
+      <div className="see-more-button" onClick={() => setProjectId(projectId)}>
+        <p>{seeMore}</p>
+        <svg
+          width="40"
+          // height="16"
+          viewBox="0 0 41 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          id="see-more-arrow"
+        >
+          <path
+            d="M40.7071 8.70711C41.0976 8.31658 41.0976 7.68342 40.7071 7.29289L34.3431 0.928932C33.9526 0.538408 33.3195 0.538408 32.9289 0.928932C32.5384 1.31946 32.5384 1.95262 32.9289 2.34315L38.5858 8L32.9289 13.6569C32.5384 14.0474 32.5384 14.6805 32.9289 15.0711C33.3195 15.4616 33.9526 15.4616 34.3431 15.0711L40.7071 8.70711ZM0 9H40V7H0V9Z"
+            // fill="white"
+          />
+        </svg>
+      </div>
     </div>
   );
 }
