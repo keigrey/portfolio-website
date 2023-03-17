8e9e47c275aae490ad2fcc4872206ef359e3cfcf
keigrey <keixgrey@gmail.com>
2023-03-17 19:08:03 +0900

    feat: Add layout components to _app to persist between pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,23 @@ import "../styles/theme.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import { appWithTranslation } from "next-i18next";
+import FilmGrain from "./components/FilmGrain";
+import NameCard from "./components/NameCard";
+import SectionCard from "./components/SectionCard";
+import Clouds from "./components/Clouds";
+import SmallMoon from "./components/SmallMoon";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <FilmGrain />
+      <div className="container-layout">
+        <SectionCard>
+          <Clouds />
+        </SectionCard>
+        <div className="name-card-placeholder"></div>
+        <SmallMoon />
+      </div>
       <Component {...pageProps} />
     </ThemeProvider>
   );
