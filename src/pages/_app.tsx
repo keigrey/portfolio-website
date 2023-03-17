078e0f7fb9b49dd12f32b50cf81b045e231d53be
keigrey <keixgrey@gmail.com>
2023-03-17 11:06:06 +0900

    fix: Fix 'Multiple instances of Three.js' error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,25 +3,8 @@ import "../styles/theme.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import { appWithTranslation } from "next-i18next";
-import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    const threeScript = document.createElement("script");
-    threeScript.setAttribute("id", "threeScript");
-    threeScript.setAttribute(
-      "src",
-      "https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"
-    );
-    document.getElementsByTagName("head")[0].appendChild(threeScript);
-
-    return () => {
-      if (threeScript) {
-        threeScript.remove();
-      }
-    };
-  }, []);
-
   return (
     <ThemeProvider>
       <Component {...pageProps} />
