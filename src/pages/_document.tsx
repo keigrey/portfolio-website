336f023f5c1a0150dd6b655dc908b6353d4718af
keigrey <keixgrey@gmail.com>
2023-03-18 15:44:40 +0900

    fix: Keep user language after page refresh

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    // TODO: lang en? Can it cause weird fonts?
-    <Html lang="en">
+    <Html>
       <Head />
       <body>
         <Main />
