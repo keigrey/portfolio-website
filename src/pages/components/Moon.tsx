cca5bee355209efcccb907c8c12d398db2c0ee5b
keigrey <keixgrey@gmail.com>
2023-03-29 18:01:41 +0900

    feat: Animate Moon and SmallMoon in and out of view

--- src/pages/components/Moon.tsx
+++ src/pages/components/Moon.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function Moon() {
-  return <div className="moon"></div>;
+  return (
+    <motion.div
+      initial={{ opacity: 1, x: "-100%", y: "-50%" }}
+      animate={{ opacity: 1, x: "-50%", y: "-50%" }}
+      exit={{ x: "300%" }}
+      transition={{ duration: 2 }}
+      className="moon"
+    ></motion.div>
+  );
 }
--- src/pages/components/SmallMoon.tsx
+++ src/pages/components/SmallMoon.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function SmallMoon() {
-  return <div className="small-moon"></div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0.18, x: "-300%", y: "-50%" }}
+      animate={{ opacity: 0.18, x: "-50%", y: "-50%" }}
+      exit={{ x: "200%" }}
+      transition={{ duration: 1 }}
+      className="small-moon"
+    ></motion.div>
+  );
 }
