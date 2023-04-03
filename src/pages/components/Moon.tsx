686f80ab4141ddfcff78b64764036b8fd4818617
keigrey <keixgrey@gmail.com>
2023-04-03 21:56:56 +0900

    feat: Ease Out the moon

--- src/pages/components/Moon.tsx
+++ src/pages/components/Moon.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { easeIn, easeOut, motion } from "framer-motion";
 
 export default function Moon() {
   return (
@@ -7,7 +7,7 @@ export default function Moon() {
       initial={{ opacity: 1, x: "-150%", y: "-50%" }}
       animate={{ opacity: 1, x: "-50%", y: "-50%" }}
       exit={{ x: "150%", transition: { duration: 1 } }}
-      transition={{ duration: 2 }}
+      transition={{ duration: 4, ease: "easeOut" }}
       className="moon"
     ></motion.div>
   );
