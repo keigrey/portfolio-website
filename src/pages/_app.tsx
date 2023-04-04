e8d8ecc77ef2446a22e7ba3642360c6414bf7d16
keigrey <keixgrey@gmail.com>
2023-04-04 14:12:12 +0900

    feat: Get screen width and add loading screen in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ import Clouds from "./components/Clouds";
 import SmallMoon from "./components/SmallMoon";
 import { motion, AnimatePresence } from "framer-motion";
 import Moon from "./components/Moon";
+import LoadingLogo from "./components/LoadingLogo";
 
 function useWindowSize() {
   // Initialize state with undefined width/height so server and client renders match
@@ -43,37 +44,80 @@ function MyApp({ Component, pageProps }: AppProps) {
   const { asPath } = useRouter();
 
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const audioPlayer = useRef<HTMLAudioElement>(null);
 
   const size = useWindowSize();
 
+  const animation = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    exit: { opacity: 0 },
+    transition: { duration: 1.5 },
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <ThemeProvider>
       <FilmGrain />
-      <div className="container-layout">
-        <SectionCard>
-          <Clouds />
-        </SectionCard>
-        <div className="name-card-placeholder"></div>
-        <AnimatePresence mode="wait">
-          {asPath === "/" ? (
-            <Moon key="moon" />
-          ) : (
-            <SmallMoon key="small-moon" />
-          )}
-        </AnimatePresence>
-      </div>
-      <audio ref={audioPlayer} src="/assets/bgm.mp3" loop></audio>
       <AnimatePresence mode="wait">
-        <Component
-          key={`${asPath}Component`}
-          {...pageProps}
-          isPlaying={isPlaying}
-          setIsPlaying={setIsPlaying}
-          audioPlayer={audioPlayer}
-          isVertical={size.width && size.width < 768}
-        />
+        {isLoading ? (
+          <motion.div
+            key="loadingScreen"
+            animate={animation.animate}
+            exit={animation.exit}
+            transition={animation.transition}
+            className="loading-screen"
+          >
+            <LoadingLogo />
+          </motion.div>
+        ) : (
+          <motion.div
+            key="content"
+            initial={animation.initial}
+            animate={animation.animate}
+            transition={animation.transition}
+          >
+            <div className="container-layout">
+              <SectionCard>
+                <Clouds />
+              </SectionCard>
+              <div className="name-card-placeholder"></div>
+              <AnimatePresence mode="wait">
+                {asPath === "/" ? (
+                  <Moon
+                    key="moon"
+                    isVertical={size.width && size.width < 768}
+                  />
+                ) : (
+                  <SmallMoon
+                    key="small-moon"
+                    isVertical={size.width && size.width < 768}
+                  />
+                )}
+              </AnimatePresence>
+            </div>
+            <audio ref={audioPlayer} src="/assets/bgm.mp3" loop></audio>
+            <AnimatePresence mode="wait">
+              <Component
+                key={`${asPath}Component`}
+                {...pageProps}
+                isPlaying={isPlaying}
+                setIsPlaying={setIsPlaying}
+                audioPlayer={audioPlayer}
+                isVertical={size.width && size.width < 768}
+              />
+            </AnimatePresence>
+          </motion.div>
+        )}
       </AnimatePresence>
     </ThemeProvider>
   );
