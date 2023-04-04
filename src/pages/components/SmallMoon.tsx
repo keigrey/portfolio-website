6641e5945d8abcb6ef77c8ac524855a01349aaca
keigrey <keixgrey@gmail.com>
2023-04-04 14:15:56 +0900

    feat: Animate SmallMoon differently based on screen orientation

--- src/pages/components/SmallMoon.tsx
+++ src/pages/components/SmallMoon.tsx
@@ -1,13 +1,27 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { anticipate, easeIn, easeInOut, easeOut, motion } from "framer-motion";
 
-export default function SmallMoon() {
+export default function SmallMoon({ isVertical }) {
+  const animation = {
+    initial: { x: "-300%", y: "-50%" },
+    animate: { x: "-50%", y: "-50%" },
+    exit: { x: "200%" },
+    transition: { duration: 1, ease: easeInOut },
+  };
+  const animationVertical = {
+    initial: { x: "-50%", y: "200%" },
+    animate: { x: "-50%", y: "-50%" },
+    exit: { y: "-300%" },
+    transition: { duration: 1, ease: easeInOut },
+  };
   return (
     <motion.div
-      initial={{ opacity: 0.18, x: "-300%", y: "-50%" }}
-      animate={{ opacity: 0.18, x: "-50%", y: "-50%" }}
-      exit={{ x: "200%" }}
-      transition={{ duration: 1 }}
+      initial={isVertical ? animationVertical.initial : animation.initial}
+      animate={isVertical ? animationVertical.animate : animation.animate}
+      exit={isVertical ? animationVertical.exit : animation.exit}
+      transition={
+        isVertical ? animationVertical.transition : animation.transition
+      }
       className="small-moon"
     ></motion.div>
   );
