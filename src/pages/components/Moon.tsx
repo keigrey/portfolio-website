8061d17e0daab35ac8bb7de4bbca25ac533bf17c
keigrey <keixgrey@gmail.com>
2023-04-04 14:15:39 +0900

    feat: Animate Moon differently based on screen orientation

--- src/pages/components/Moon.tsx
+++ src/pages/components/Moon.tsx
@@ -1,13 +1,28 @@
 import React from "react";
-import { easeIn, easeOut, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
-export default function Moon() {
+export default function Moon({ isVertical }) {
+  const animation = {
+    initial: { opacity: 1, x: "-120%", y: "-50%" },
+    animate: { opacity: 1, x: "-50%", y: "-50%" },
+    exit: { x: "150%", transition: { duration: 1 } },
+    transition: { duration: 4, ease: "easeInOut" },
+  };
+  const animationVertical = {
+    initial: { opacity: 1, x: "-50%", y: "10%" },
+    animate: { opacity: 1, x: "-50%", y: "-50%" },
+    exit: { y: "-250%", transition: { duration: 1 } },
+    transition: { duration: 4, ease: "easeInOut" },
+  };
+
   return (
     <motion.div
-      initial={{ opacity: 1, x: "-150%", y: "-50%" }}
-      animate={{ opacity: 1, x: "-50%", y: "-50%" }}
-      exit={{ x: "150%", transition: { duration: 1 } }}
-      transition={{ duration: 4, ease: "easeOut" }}
+      initial={isVertical ? animationVertical.initial : animation.initial}
+      animate={isVertical ? animationVertical.animate : animation.animate}
+      exit={isVertical ? animationVertical.exit : animation.exit}
+      transition={
+        isVertical ? animationVertical.transition : animation.transition
+      }
       className="moon"
     ></motion.div>
   );
